2976c510fcbbb44332c2fb0acace7bbcf96b68a0
Kadin Zhang <zhangkadin@gmail.com>
2018-09-02 20:06:33 -0400

    redirect http to https through express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var secure = require('express-force-https');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(secure);
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, (req, res) => {
   res.sendfile(__dirname + "/dist/index.html");
